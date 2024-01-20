be958db396f62d9e99cde8a4072acd7994f4d770
dds-dhruvi <dhruvids@gmail.com>
2024-01-20 11:26:51 +0530

    decorator pattern code

---
+++ src/decoratorPattern/DecoratorPattern.java
@@ -0,0 +1,9 @@
+package src.decoratorPattern;
+
+public class DecoratorPattern {
+    public static void main(String[] args)  //static method
+    {
+        System.out.println("Static method");
+    }
+
+}
