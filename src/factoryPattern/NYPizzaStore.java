3a602a7077c30bf54e3473784a4e33b47f78223c
dds-dhruvi <dhruvids@gmail.com>
2024-01-28 23:35:27 +0530

    factory pattern added

---
+++ src/factoryPattern/NYPizzaStore.java
@@ -0,0 +1,11 @@
+package src.factoryPattern;
+
+public class NYPizzaStore extends PizzaStore{
+
+
+    @Override
+    protected Pizza createPizza(String type) {
+        return null;
+    }
+    
+}
---
+++ src/factoryPattern/Pizza.java
@@ -0,0 +1,21 @@
+package src.factoryPattern;
+
+public class Pizza {
+
+    String name;
+    String toppings;
+
+    void bake(){
+        System.out.println("baking the pizza");
+    }
+
+    void cut(){
+        System.out.println("cutting the pizza");
+    }
+
+    void pack(){
+        System.out.println("packing the pizza");
+    }
+
+
+}
